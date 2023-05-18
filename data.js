3973dc7957545ea3be1df914e9842d8bc99324d9
yurii433 <yuradiachuk1@gmail.com>
2023-05-18 13:45:42 +0300

    v.1.0

--- data.js
+++ data.js
@@ -2,21 +2,21 @@ export const menuArray = [
     {
         name: "Pizza",
         ingredients: ["pepperoni", "mushrom", "mozarella"],
-        id: 0,
         price: 14,
-        emoji: "🍕"
+        emoji: "🍕",
+        id: 0,
     },
     {
         name: "Hamburger",
         ingredients: ["beef", "cheese", "lettuce"],
-        price: 12,
+        price: 11,
         emoji: "🍔",
         id: 1
     },
         {
         name: "Beer",
         ingredients: ["grain, hops, yeast, water"],
-        price: 7,
+        price: 6,
         emoji: "🍺",
         id: 2
     }
--- index.js
+++ index.js
@@ -1,27 +1,115 @@
 import { menuArray } from '/data.js';
 
 const menuEl = document.getElementById("menu")
+const orderEl = document.getElementById("order")
+const cardDetailsEl = document.getElementById("card-details-wrap")
+let itemsOrdered = []
 
+document.addEventListener("click", function(e){
+    if(e.target.dataset.additembtn){
+        addItem(Number(e.target.dataset.additembtn))
+    }
+    else if (e.target.dataset.removebtn) {
+        removeItem(e.target.dataset.removebtn)
+    }
+    else if (e.target.id === "btn-order")
+    {
+      completeOrder()
+    }
+    else if(e.target.id === "btn-pay"){
+        e.preventDefault()
+        processPayment()
+    }
+})
 
-function render() {
+function addItem(itemId){
+    const targetMenuItem = menuArray.filter(function(item){
+    return item.id === Number(itemId);
+   })[0]
 
+    itemsOrdered.push({
+    name: targetMenuItem.name,
+    price: targetMenuItem.price,
+    id: getRandomId(),
+   })
+   renderMenu()
+}
+
+function removeItem(removeBtnId){
+    const removeTargetObject = itemsOrdered.filter(function(rmObj){
+        return rmObj.id === Number(removeBtnId);
+    })[0]
+    itemsOrdered.splice(itemsOrdered.indexOf(removeTargetObject), 1)
+    renderMenu()
+
+}
+
+function renderMenu() {
+    let menu = ``
         for (const item of menuArray){
-  
-        menuEl.innerHTML += 
-        `<div class="object">
-            <div class="object-wrap">
-                <div class="object-logo">${item.emoji}</div>
-                <div class="object-info">
-                <div class="object-info-name">${item.name}</div>
-                <div class="object-info-ingridients">${Array.isArray(item.ingredients) ? item.ingredients.join(", ") : item.ingredients}</div>
-                <div class="object-info-price">$${item.price}</div>     
+          menu  += 
+                `<div class="object">
+                    <div class="object-wrap">
+                        <div class="object-logo">${item.emoji}</div>
+                        <div class="object-info">
+                        <div class="object-info-name">${item.name}</div>
+                        <div class="object-info-ingridients">${Array.isArray(item.ingredients) ? item.ingredients.join(", ") : item.ingredients}</div>
+                        <div class="object-info-price">$${item.price}</div>     
+                        </div>
+                        <button class="object-button" data-additembtn = "${item.id}">+</button>
+                    </div>
+                    <hr>
+                </div>`;
+
+                menuEl.innerHTML = menu;
+                
+                let orderItemsHTML = ``
+                let totalPrice = 0;
+
+                itemsOrdered.length > 0 ? orderEl.classList.remove("hide") : orderEl.classList.add("hide");
+
+                for (let item of itemsOrdered) {
+                    orderItemsHTML += 
+                ` <div class="order-item">
+                        <div class="order-item-name">${item.name}</div>
+                        <button class="order-item-remove" data-removebtn = "${item.id}">remove</button>
+                        <div class="order-item-price">${item.price}</div>
+                    </div>`
+                    totalPrice += item.price;
+                }
+
+                orderEl.innerHTML = `<div class="order-wrap">
+                <div class="order-header">
+                    <h2>Your order</h2>
                 </div>
-                <button class="object-button">+</button>
-            </div>
-            <hr>
-        </div>`;
+                ${orderItemsHTML}
+                <hr class="hr-bottom">
+                <div class="total-price-wrap">
+                    <div class="total-price-text">Total price</div>
+                    <div class="total-price order-item-price" id="total-price">${totalPrice}</div>
+                </div>
+                <button class="btn-order" id="btn-order">Complete order</button>
+                </div>` 
+
+
 }}
 
-render()
+function completeOrder() {
+    cardDetailsEl.classList.toggle("hide")
+    menuEl.style.filter ='blur(1px)' 
+    orderEl.style.filter = 'blur(1px)'
+}
 
+function processPayment() {
+    cardDetailsEl.classList.toggle("hide")
+    menuEl.style.filter ='none' 
+    orderEl.style.filter = 'none'
+    itemsOrdered = []
+    orderEl.innerHTML = `<div class="final-message" id="final-message">Thanks, ${document.getElementById("client-name").value}! Your order is on its way!</div>`
+}
 
+function getRandomId() {
+   return Math.floor(Math.random()*1000)
+}
+
+renderMenu()
